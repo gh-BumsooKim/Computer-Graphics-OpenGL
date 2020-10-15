88e7070287ebfc1c1647bf919fda0fb756f782db
Bumsoo Kim <cmng828rhuypqq@cau.ac.kr>
2020-10-16 01:10:48 +0900

    Add files via upload

---
+++ W1_CGProject/W1_CGProject/W5-4_CGProject.cpp
@@ -0,0 +1,43 @@
+#include <GL/glut.h>
+
+GLfloat Delta = 0.0;
+
+void MyDisplay()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_POLYGON);
+	glColor3f(0.0, 0.5, 0.8);
+	glVertex3f(-1.0 + Delta, -0.5, 0.0);
+	glVertex3f(0.0 + Delta, -0.5, 0.0);
+	glVertex3f(0.0 + Delta, 0.5, 0.0);
+	glVertex3f(-1.0 + Delta, 0.5, 0.0);
+	glEnd();
+	glutSwapBuffers();
+}
+
+void MyIdle()
+{
+	Delta += 0.01;
+	if (Delta > 2.0) Delta = 0.0;
+	glutPostRedisplay();
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GL_DOUBLE);
+	glutInitWindowSize(300, 300);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("W5-4_CGProject");
+
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+
+	glutDisplayFunc(MyDisplay);
+	glutIdleFunc(MyIdle);
+	glutMainLoop();
+
+	return 0;
+}
